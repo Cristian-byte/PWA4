4e3bc0317746424637ff3f500b3ff57abb6eb30e
Cristian Sanchez Flores <andygign40@gmail.com>
2022-10-02 20:06:26 -0500

    Formas válidas para realizar peticiones desde el evento Fetch

--- sw.js
+++ sw.js
@@ -1,11 +1,24 @@
 //console.log('SW: Hola mundo 2');
 
-self.addEventListener('fetch', event => {
+/*self.addEventListener('fetch', event => {
 
     if (event.request.url.includes('style.css')) {
         event.respondWith(null);
     } else {
 
         event.respondWith(fetch(event.request));
+    }
+});*/
+
+self.addEventListener('fetch', event => {
+
+    if (event.request.url.includes('.jpg')) {
+        console.log(event.request.url);
+
+        //let fotoReq = fetch('img/main.jpg');
+        //let fotoReq = fetch(event.request.url);
+        let fotoReq = fetch(event.request);
+
+        event.respondWith(fotoReq);
     }
 });
