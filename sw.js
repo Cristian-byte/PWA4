d3d90d4e809383e623b9e468efb1e17be3a391a1
Cristian Sanchez Flores <andygign40@gmail.com>
2022-10-02 20:27:03 -0500

    Manejo de errores en el Fetch Event

--- sw.js
+++ sw.js
@@ -41,7 +41,7 @@
     }
 });*/
 
-self.addEventListener('fetch', event => {
+/*self.addEventListener('fetch', event => {
     if (event.request.url.includes('main.jpg')) {
 
         //event.respondWith(fetch('img/main-patas-arriba.jpg'));
@@ -49,4 +49,16 @@ self.addEventListener('fetch', event => {
         let resp = fetch('img/main-patas-arriba.jpg');
         event.respondWith(resp);
     }
+});*/
+
+self.addEventListener('fetch', event => {
+    const resp = fetch(event.request)
+        .then(resp => {
+            
+            //if{resp.ok} return resp;
+            //else return fetch('img/main.jpg');
+            return resp.ok ? resp : fetch('img/main.jpg');
+        });
+        
+    event.respondWith(resp);
 });
